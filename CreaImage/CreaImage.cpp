6dc6450d8773cba2b19a3084bb2b1886b7b3919b
JulienPercheron <julien_p@icloud.com>
2022-09-27 15:58:13 +0200

    Ajout des cubes englobants

--- CreaImage/CreaImage.cpp
+++ CreaImage/CreaImage.cpp
@@ -33,6 +33,104 @@ struct Sphere {
 	}
 };
 
+struct CubeEnglobant {
+	Vec3d min;
+	Vec3d max;
+
+	vector<Sphere> listSpheres;
+
+	bool intersect(const Ray& r) const
+	{
+		float tmin, tmax, tymin, tymax, tzmin, tzmax;
+
+		Vec3d invdir = { 1 / r.direction[0], 1 / r.direction[1], 1 / r.direction[2] };
+
+		if (invdir[0] >= 0) {
+			tmin = (min[0] - r.origin[0]) * invdir[0];
+			tmax = (max[0] - r.origin[0]) * invdir[0];
+		}
+		else {
+			tmin = (max[0] - r.origin[0]) * invdir[0];
+			tmax = (min[0] - r.origin[0]) * invdir[0];
+		}
+		if (invdir[1] >= 0) {
+			tymin = (min[1] - r.origin[1]) * invdir[1];
+			tymax = (max[1] - r.origin[1]) * invdir[1];
+		}
+		else {
+			tymin = (max[1] - r.origin[1]) * invdir[1];
+			tymax = (min[1] - r.origin[1]) * invdir[1];
+		}
+		if (invdir[2] >= 0) {
+			tzmin = (min[2] - r.origin[2]) * invdir[2];
+			tzmax = (max[2] - r.origin[2]) * invdir[2];
+		}
+		else {
+			tzmin = (max[2] - r.origin[2]) * invdir[2];
+			tzmax = (min[2] - r.origin[2]) * invdir[2];
+		}
+		
+		if ((tmin > tymax) || (tymin > tmax))
+			return false;
+
+		if (tymin > tmin)
+			tmin = tymin;
+		if (tymax < tmax)
+			tmax = tymax;
+
+		
+		if ((tmin > tzmax) || (tzmin > tmax))
+			return false;
+
+		if (tzmin > tmin)
+			tmin = tzmin;
+		if (tzmax < tmax)
+			tmax = tzmax;
+
+		return true;
+	}
+
+	void spheresInCube(vector<Sphere> spheres) {
+		double maxX = max[0];
+		double maxY = max[1];
+		double maxZ = max[2];
+
+		double minX = min[0];
+		double minY = min[1];
+		double minZ = min[2];
+		for (Sphere& s : spheres) {
+			if (s.origin[0] > min[0] && s.origin[0] < max[0] && s.origin[1] > min[1] && s.origin[1] < max[1] && s.origin[2] > min[2] && s.origin[2] < max[2]) {
+				listSpheres.push_back(s);
+
+				if (s.origin[0] + s.rayon > maxX)
+					maxX = s.origin[0] + s.rayon;
+				if (s.origin[1] + s.rayon > maxY)
+					maxY = s.origin[1] + s.rayon;
+				if (s.origin[2] + s.rayon > maxZ)
+					maxZ = s.origin[2] + s.rayon;
+				
+				if (s.origin[0] - s.rayon < minX)
+					minX = s.origin[0] - s.rayon;
+				if (s.origin[1] - s.rayon < minY)
+					minY = s.origin[1] - s.rayon;
+				if (s.origin[2] - s.rayon < minZ)
+					minZ = s.origin[2] - s.rayon;
+
+			}
+		}
+
+		min[0] = minX;
+		min[1] = minY;
+		min[2] = minZ;
+
+		max[0] = maxX;
+		max[1] = maxY;
+		max[2] = maxZ;
+	}
+
+
+};
+
 
 struct LightSource {
 	Vec3d origin;
@@ -58,87 +156,168 @@ int main()
 
 
 	float sphereDistance = 255;
+	float tailleSphere = 40;
 
-	//Sphere murFond{ Vec3d{500, sphereDistance+500, 500}, 400, {1,0,0} };
-	//Sphere murGauche{ Vec3d{-10000, sphereDistance+30, 500}, 93230, {0,0,1} };
-	//Sphere murDroite{ Vec3d{11000, sphereDistance+30, 500}, 93230, {0,0,1} };
-	//Sphere murHaut{ Vec3d{500, sphereDistance + 30, -10000}, 93230, {0,1,0} };
-	Sphere murBas{ Vec3d{400, sphereDistance - 100, 450}, 20, {0,1,0} };
-	Sphere sphere{ Vec3d{350, sphereDistance, 500}, 100 , {0,1,1} };
-	Sphere sphere2{ Vec3d{650, sphereDistance, 500}, 100, {1,1,0} };
+	Sphere sphere11{ Vec3d{100, sphereDistance, 100}, tailleSphere, {0,1,0} };
+	Sphere sphere12{ Vec3d{200, sphereDistance, 100}, tailleSphere , {0,1,1} };
+	Sphere sphere13{ Vec3d{300, sphereDistance, 100}, tailleSphere, {1,1,0} };
+	Sphere sphere14{ Vec3d{400, sphereDistance, 100}, tailleSphere, {0,1,0} };
+	Sphere sphere15{ Vec3d{500, sphereDistance, 100}, tailleSphere , {0,1,1} };
+	Sphere sphere16{ Vec3d{600, sphereDistance, 100}, tailleSphere, {1,1,0} };
+	Sphere sphere17{ Vec3d{700, sphereDistance, 100}, tailleSphere, {0,1,0} };
+	Sphere sphere18{ Vec3d{800, sphereDistance, 100}, tailleSphere , {0,1,1} };
+	Sphere sphere19{ Vec3d{900, sphereDistance, 100}, tailleSphere, {1,1,0} };
+	Sphere sphere21{ Vec3d{100, sphereDistance, 200}, tailleSphere, {0,1,0} };
+	Sphere sphere22{ Vec3d{200, sphereDistance, 200}, tailleSphere , {0,1,1} };
+	Sphere sphere23{ Vec3d{300, sphereDistance, 200}, tailleSphere, {1,1,0} };
+	Sphere sphere24{ Vec3d{400, sphereDistance, 200}, tailleSphere, {0,1,0} };
+	Sphere sphere25{ Vec3d{500, sphereDistance, 200}, tailleSphere , {0,1,1} };
+	Sphere sphere26{ Vec3d{600, sphereDistance, 200}, tailleSphere, {1,1,0} };
+	Sphere sphere27{ Vec3d{700, sphereDistance, 200}, tailleSphere, {0,1,0} };
+	Sphere sphere28{ Vec3d{800, sphereDistance, 200}, tailleSphere , {0,1,1} };
+	Sphere sphere29{ Vec3d{900, sphereDistance, 200}, tailleSphere, {1,1,0} };
+	Sphere sphere31{ Vec3d{100, sphereDistance, 300}, tailleSphere, {0,1,0} };
+	Sphere sphere32{ Vec3d{200, sphereDistance, 300}, tailleSphere , {0,1,1} };
+	Sphere sphere33{ Vec3d{300, sphereDistance, 300}, tailleSphere, {1,1,0} };
+	Sphere sphere34{ Vec3d{400, sphereDistance, 300}, tailleSphere, {0,1,0} };
+	Sphere sphere35{ Vec3d{500, sphereDistance, 300}, tailleSphere , {0,1,1} };
+	Sphere sphere36{ Vec3d{600, sphereDistance, 300}, tailleSphere, {1,1,0} };
+	Sphere sphere37{ Vec3d{700, sphereDistance, 300}, tailleSphere, {0,1,0} };
+	Sphere sphere38{ Vec3d{800, sphereDistance, 300}, tailleSphere , {0,1,1} };
+	Sphere sphere39{ Vec3d{900, sphereDistance, 300}, tailleSphere, {1,1,0} };
+	Sphere sphere91{ Vec3d{100, sphereDistance, 900}, tailleSphere, {0,1,0} };
+	Sphere sphere92{ Vec3d{200, sphereDistance, 900}, tailleSphere , {0,1,1} };
+	Sphere sphere93{ Vec3d{300, sphereDistance, 900}, tailleSphere, {1,1,0} };
+	Sphere sphere94{ Vec3d{400, sphereDistance, 900}, tailleSphere, {0,1,0} };
+	Sphere sphere95{ Vec3d{500, sphereDistance, 900}, tailleSphere , {0,1,1} };
+	Sphere sphere96{ Vec3d{600, sphereDistance, 900}, tailleSphere, {1,1,0} };
+	Sphere sphere97{ Vec3d{700, sphereDistance, 900}, tailleSphere, {0,1,0} };
+	Sphere sphere98{ Vec3d{800, sphereDistance, 900}, tailleSphere , {0,1,1} };
+	Sphere sphere99{ Vec3d{900, sphereDistance, 900}, tailleSphere, {1,1,0} };
 
 
 	//vector<Sphere> spheres = { sphere, sphere2, murGauche, murDroite, murHaut, murBas, murFond };
-	vector<Sphere> spheres = { murBas, sphere, sphere2 };
+	vector<Sphere> spheres = { 
+		sphere11, sphere12, sphere13, sphere14, sphere15, sphere16, sphere17, sphere18, sphere19,
+		sphere21, sphere22, sphere23, sphere24, sphere25, sphere26, sphere27, sphere28, sphere29,
+		sphere31, sphere32, sphere33, sphere34, sphere35, sphere36, sphere37, sphere38, sphere39,
+		sphere91, sphere92, sphere93, sphere94, sphere95, sphere96, sphere97, sphere98, sphere99,
+	};
 
 	LightSource lightSource{ Vec3d{500, sphereDistance - 200, 300}, 70000000 };
 	
+	CubeEnglobant initialCube = {Vec3d{0, 1 ,0}, Vec3d{imgWidth, sphereDistance + 20, imgHeight} };
+
+	vector<CubeEnglobant> cubes = { initialCube };
+
+	vector<CubeEnglobant> tempCubes;
+	for (int indice = 0; indice < 2; ++indice) {
+		tempCubes.clear();
+		for (CubeEnglobant& cube : cubes) {
+			if (cube.max[0] - cube.min[0] > cube.max[1] - cube.min[1] && cube.max[0] - cube.min[0] > cube.max[2] - cube.min[2]) {
+				CubeEnglobant cube1 = { Vec3d{cube.min[0], cube.min[1], cube.min[2]}, Vec3d{cube.max[0]/2, cube.max[1] ,cube.max[2]} };
+				CubeEnglobant cube2 = { Vec3d{cube.max[0]/2, cube.min[1], cube.min[2]}, Vec3d{cube.max[0], cube.max[1] ,cube.max[2]} };
+				 
+				tempCubes.push_back(cube1);
+				tempCubes.push_back(cube2);
+			}
+			else if (cube.max[1] - cube.min[1] > cube.max[0] - cube.min[0] && cube.max[1] - cube.min[1] > cube.max[2] - cube.min[2]){
+				CubeEnglobant cube1 = { Vec3d{cube.min[0], cube.min[1], cube.min[2]}, Vec3d{cube.max[0], cube.max[1] / 2 ,cube.max[2]} };
+				CubeEnglobant cube2 = { Vec3d{cube.min[0], cube.max[1] / 2, cube.min[2]}, Vec3d{cube.max[0], cube.max[1] ,cube.max[2]} };
+
+				tempCubes.push_back(cube1);
+				tempCubes.push_back(cube2);
+			} 
+			else {
+				CubeEnglobant cube1 = { Vec3d{cube.min[0], cube.min[1], cube.min[2]}, Vec3d{cube.max[0], cube.max[1] ,cube.max[2]/2} };
+				CubeEnglobant cube2 = { Vec3d{cube.min[0], cube.min[1], cube.max[2] / 2}, Vec3d{cube.max[0], cube.max[1] ,cube.max[2]} };
+
+				tempCubes.push_back(cube1);
+				tempCubes.push_back(cube2);
+			}
+		}
+		cubes = tempCubes;
+	}
+
 	bool sphereFound = false;
 	for (int i = 0; i < imgWidth; ++i) {
 		for (int j = 0; j < imgHeight; ++j) {
 
-			Vec3d rayDirection = Vec3d{ (float)i, 0, (float)j }-origine;
-			double normRayDirection = cv::norm(rayDirection, NORM_L2);
-			rayDirection = rayDirection / normRayDirection;
+			//Vec3d rayDirection = Vec3d{ (float)i, 0, (float)j }-origine;
+			//double normRayDirection = cv::norm(rayDirection, NORM_L2);
+			//rayDirection = rayDirection / normRayDirection;
 
-			//Vec3d rayDirection = Vec3d{ 0, 1, 0 };
+			Vec3d rayDirection = Vec3d{ 0, 1, 0 };
 
 			Ray ray{ Vec3d{(float)i, 0, (float)j}, rayDirection };
-			for (Sphere& s : spheres) {
-				if (sphereFound)
-					break;
 
-				float res = s.intersect(ray);
+			
+			for (CubeEnglobant cube : cubes) {
+				if (cube.intersect(ray)) {
+					for (Sphere& s : spheres) {
+						if (sphereFound)
+							break;
 
-
-
-				if (res > 0) {
-					Vec3d X = ray.origin + res * ray.direction;
-					Vec3d directionLampe = lightSource.origin - X;
-					double d2 = pow(directionLampe[0], 2) + pow(directionLampe[1], 2) + pow(directionLampe[2], 2);
-					Vec3d w0 = directionLampe / sqrt(d2);
+						float res = s.intersect(ray);
 
 
+						if (res > 0) {
+							Vec3d X = ray.origin + res * ray.direction;
+							Vec3d directionLampe = lightSource.origin - X;
+							double d2 = pow(directionLampe[0], 2) + pow(directionLampe[1], 2) + pow(directionLampe[2], 2);
+							Vec3d w0 = directionLampe / sqrt(d2);
 
-					Vec3d normale = X - s.origin;
-					double normNormale = cv::norm(normale, NORM_L2);
-					normale = normale / normNormale;
-					double f = abs(w0.dot(normale) / PI);
 
-					//cout << w0 << endl;
-					Ray rayFromIntersec{ X, w0 };
-					double smallestDist = -1;
 
-					for (Sphere& s2 : spheres) {
-						double dist = s2.intersect(rayFromIntersec);
-						if (smallestDist == -1 && dist > 0)
-							smallestDist = dist;
-						else if (smallestDist > dist && dist > 0)
-							smallestDist = dist;
+							Vec3d normale = X - s.origin;
+							double normNormale = cv::norm(normale, NORM_L2);
+							normale = normale / normNormale;
+							double f = abs(w0.dot(normale) / PI);
+
+							//cout << w0 << endl;
+							Ray rayFromIntersec{ X, w0 };
+							double smallestDist = -1;
+
+							for (Sphere& s2 : spheres) {
+								double dist = s2.intersect(rayFromIntersec);
+								if (smallestDist == -1 && dist > 0)
+									smallestDist = dist;
+								else if (smallestDist > dist && dist > 0)
+									smallestDist = dist;
+							}
+
+							float isClear = 1;
+							//cout << abs(pow(smallestDist, 2) - d2)  << endl;
+							if (smallestDist != -1) {
+								if (pow(smallestDist, 2) > d2)
+									isClear = 1;
+								else
+									isClear = 0;
+							}
+
+
+							float couleur = (lightSource.color * f * isClear) / d2;
+
+							if (couleur > 255) {
+								couleur = 255;
+							}
+							else if (couleur < 0) {
+								couleur = 0;
+							}
+
+							image.at<Vec3b>({ i,j }) = couleur * s.albedo;
+							sphereFound = true;
+						}
+						else {
+							image.at<Vec3b>({ i,j }) = { 150,150,150 };
+						}
+
+
 					}
 
-					float isClear = 1;
-					//cout << abs(pow(smallestDist, 2) - d2)  << endl;
-					if (smallestDist != -1) {
-						if (pow(smallestDist, 2) > d2)
-							isClear = 1;
-						else
-							isClear = 0;
-					}
-
-
-					Vec3d couleur = (lightSource.color * f * s.albedo * isClear) / d2;
-
-					image.at<Vec3b>({ i,j }) = couleur;
-					sphereFound = true;
 				}
-				else {
-					image.at<Vec3b>({ i,j }) = { 150,150,150 };
-				}
-
-
+				sphereFound = false;
 			}
-			sphereFound = false;
 		}
 	}
 	
